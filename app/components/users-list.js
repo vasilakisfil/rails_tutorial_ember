ff5f7708f46f9df6146e6eeccc529321fb0bfd41
Filippos Vasilakis <vasilakisfil@gmail.com>
2016-05-06 09:28:01 +0200

    wip

--- app/components/users-list.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  showTopPageNumbers: true,
-  showBottomPageNumbers: true
-});
--- app/controllers/login.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
-
-export default Ember.Controller.extend(LoginControllerMixin, {
-  authenticator: 'simple-auth-authenticator:devise',
-});
-
-
